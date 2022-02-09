7ca3004c82651265934e61859c3cadf865f93039
in2(akagawa) <hideyuki.aka@gmail.com>
2022-02-09 20:04:21 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import contractAbi from "./contracts/Counter.json"
+import {ethers} from "ethers";
+
+//contract address
+const contractAddress = "0x4800Cf28C598BA85a3465a3e49AE94e94503A4fC";
+const abi = contractAbi.abi;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const singer = provider.getSigner();
+const contract = new ethers.Contract(contractAddress, abi, singer);
 
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("No Wallet Connection!");
+  const [count, setCount] = useState("no count");
+
+  contract.on("UpdatedCount", (from, to, amount, event) => {
+    setCount(parseInt(from, 10))
+  });
+
+  const connectWalletHandler = async () => {
+    if (!window.ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We are ready to go!");
+    }
+
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const inc = async () => {
+    try {
+      await contract.inc(walletAddress);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const dec = async () => {
+    try {
+      await contract.dec(walletAddress);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const get = async () => {
+    const c = await contract.get();
+    setCount(parseInt(c, 10));
+    console.log(c);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Counter Dapp</h1>
+        <p>ContractAddress: { contractAddress }</p>
+        <p>Wallet Address: { walletAddress }</p>
+        <p>count: { count }</p>
+        <p>incrementを押すとcountが１増えます。decrementを押すとcountが１減ります。countはコントラクト全体で共有されていて、getで現在の値がわかります。</p>
+        <p>Ropsten Test Network に設定してください。それ以外は反応しないです。</p>
+        <button onClick={connectWalletHandler}>Connect Wallet</button>
+        <button onClick={inc}>increment</button>
+        <button onClick={dec}>decrement</button>
+        <button onClick={get}>get</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
